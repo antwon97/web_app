5522746dcc4a004e9f887af1de451405d310e87a
Anthony <antwon97@gmail.com>
2021-05-15 22:55:32 -0500

    Implement part 2

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -7,9 +7,32 @@ namespace web_app.Controllers
     {
         // GET: /<controller>/
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return Content("<h1>Hello World</h1>", "text/html");
+            /*
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+            */
+
+            return Redirect("/Hello/Goodbye");
+        }
+
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // Handle requests to /Hello/Tim (url segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
         }
 
         public IActionResult Goodbye()
